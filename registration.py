9e69c4ecbe1b79ad86602e87ed563edd9d58a0ee
Matvii Golovko <cer6erus7@Matviis-Laptop.lan>
2023-07-20 13:28:18 -0700

    Final version of this project!

--- registration.py
+++ registration.py
@@ -2,15 +2,20 @@ import utilities
 
 
 help = """/register, /login, /help, /end"""
-
+point = True
+log_login = None
 print(help)
-while True:
+
+
+while point:
     lobby = input('Type command here - ')
+
     if lobby == '/help':
         print(help)
+
     elif lobby == '/register':
         while True:
-            login = input('Type your login - ')
+            login = input('Type your new login - ')
             password = input('Type your password - ')
             password1 = input('Type your password again - ')
             if utilities.chek_login(login):
@@ -22,3 +27,27 @@ while True:
                     print('Passwords are different! Try again!')
             else:
                 print("Login is already exist! Try again!")
+
+    elif lobby == "/login":
+        while True:
+            login = input("Type your login - ")
+            password = input('Type your password - ')
+            if utilities.get_login_password(login, password):
+                print("You are in!\n")
+                point = False
+                log_login = login
+                break
+            else:
+                print("Your date is incorrect! Try again")
+
+    elif lobby == '/end':
+        print('GoodBye!')
+        point = False
+
+    else:
+        print("Command is incorrect!")
+
+
+
+if log_login:
+    print(utilities.loged_login(log_login))
--- utilities.py
+++ utilities.py
@@ -22,7 +22,38 @@ def chek_login(login):
                 return False
         return True
 
+def get_login_password(login, password):
+    """
+    Принимает логин и пароль, и проверяет их в базе даных. Возвращает Правду, если логин
+    и пароль есть в наличии, а если нет то, Ложь
+    :param login:
+    :param password:
+    :return:
+    """
+    with open('db', 'r') as f:
+        for i in f.readlines():
+            lst = i.split()
+            if lst[0] == login and lst[1] == password:
+                return True
+        return False
+
+
+def loged_login(login):
+    """
+    Принимает логин и приветствует залогиневшегося клиента
+    :param login:
+    :return:
+    """
+    with open('db', 'r') as f:
+        for i in f.readlines():
+            lst = i.split()
+            if lst[0] == login:
+                return f"Hello {lst[0]}!"
+        return "Login doesn't exist!"
+
 
 if __name__ == "__main__":
-    register('Matvey', '12342')
+    # register('Matvey', '12342')
     print(chek_login('Matvey'))
+    print(get_login_password('Matvey', '1232'))
+    print(loged_login('Matvey'))
